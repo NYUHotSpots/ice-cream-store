2445335527fad07686b03fa4122c6b68447300ef
Tiffany Chan <tc2965@nyu.edu>
2021-11-03 16:20:59 +0000

    try path without ~

--- db/db.py
+++ db/db.py
@@ -7,16 +7,18 @@ Gradually, we will fill in actual calls to our datastore.
 import json
 import os
 
-path = os.environ.get("DB_Path")
-ICE_CREAM_DB = f"{path}/db/ice_cream.json"
+DB_Path = os.environ["DB_Path"]
+ICE_CREAM_DB = f"{DB_Path}/db/ice_cream.json"
 
 
 def get_ice_cream():
     """
     A function to return all chat rooms.
     """
+    print(DB_Path)
+    print(ICE_CREAM_DB)
     try:
-        with open(ICE_CREAM_DB) as file:
+        with open(ICE_CREAM_DB[1:]) as file:
             return json.loads(file.read())
     except FileNotFoundError:
         return None
